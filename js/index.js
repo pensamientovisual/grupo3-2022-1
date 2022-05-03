f8c3b4c63c494ef3e4ed751e694f4fb227ab368f
rgaona <rgaona@pop-os.localdomain>
2022-05-03 12:19:04 -0400

    :sparkles: Added more countries and changed color axis

--- js/index.js
+++ js/index.js
@@ -8,7 +8,7 @@
         'https://code.highcharts.com/mapdata/custom/world.topo.json'
     ).then(response => response.json());
 
-    Highcharts.getJSON(window.location.href + "data/csvjson.json", function (data) {
+    Highcharts.getJSON(window.location.href + "/data/spendings.json", function (data) {
 
         // Initialize the chart
         Highcharts.mapChart('container', {
@@ -28,6 +28,21 @@
                 }
             },
 
+            legend: {
+                title: {
+                    text: 'Gasto gubernamental en aerolineas en MM de dolares',
+                    style: {
+                        color: ( // theme
+                            Highcharts.defaultOptions &&
+                            Highcharts.defaultOptions.legend &&
+                            Highcharts.defaultOptions.legend.title &&
+                            Highcharts.defaultOptions.legend.title.style &&
+                            Highcharts.defaultOptions.legend.title.style.color
+                        ) || 'black'
+                    }
+                }
+            },
+
             tooltip: {
                 backgroundColor: 'none',
                 borderWidth: 0,
@@ -44,8 +59,14 @@
 
             colorAxis: {
                 min: 0.1,
-                max: 100,
-                type: 'logarithmic'
+                type: 'logarithmic',
+                minColor: '#EEEEFF',
+                maxColor: '#000022',
+                stops: [
+                    [0, '#EFEFFF'],
+                    [0.8, '#4444FF'],
+                    [1, '#000022']
+                ]
             },
 
             series: [{
@@ -62,3 +83,17 @@
     });
 
 })();
+
+
+/* Inicio bibliografia datos 
+
+Portugal: 
+ - https://www.reuters.com/business/portugals-tap-may-need-more-aid-than-planned-2021-minister-says-2021-02-14/
+
+Spain, GB, Greece, Netherlands, France, Ireland, Italy, Germany, Finland, Sweden:
+- https://assets.gov.ie/205030/a6e0e95a-2486-433c-9af4-0aaaa2362788.pdf
+
+OECD countries:
+- https://www.oecd.org/corporate/State-Support-to-the-Air-Transport-Sector-Monitoring-Developments-Related-to-the-COVID-19-Crisis.pdf
+
+*/
